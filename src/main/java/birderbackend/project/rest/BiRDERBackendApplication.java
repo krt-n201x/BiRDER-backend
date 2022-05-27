1852a2d49821c7a5a610cca85d3bdf60986c2ca9
NNilawan <nilawan_l@cmu.ac.th>
2022-05-28 02:22:37 +0700

    initial project

---
+++ src/main/java/birderbackend/project/rest/BiRDERBackendApplication.java
@@ -0,0 +1,13 @@
+package birderbackend.project.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiRDERBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiRDERBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/birderbackend/project/rest/BiRDERBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package birderbackend.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiRDERBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
